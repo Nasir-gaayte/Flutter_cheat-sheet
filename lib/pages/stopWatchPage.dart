53f15b733c2963d907963fec07e308987ee5379c
Nasir-gaayte <slowboyz12@gmail.com>
2024-05-14 16:54:32 +0300

    14/5update5

--- lib/pages/stopWatchPage.dart
+++ lib/pages/stopWatchPage.dart
@@ -13,13 +13,41 @@ class _StopWatchPageState extends State<StopWatchPage> {
   Duration _duration = Duration();
   Timer? _timer;
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startTimer();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Center(child: buildTimer());
   }
 
   buildTimer() {
     return Row(
-      children: [],
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(_duration.inHours.toString()),
+        Text(_duration.inMinutes.toString()),
+        Text(_duration.inSeconds.toString()),
+        Text(_duration.inMicroseconds.toString()),
+      ],
     );
+  }
+
+  void startTimer() {
+    var addSecond = 1;
+    _timer = Timer.periodic(
+      Duration(seconds: 1),
+      (timer) {
+        addTimer();
+      },
+    );
+  }
+
+  void addTimer() {
+    _duration = Duration(seconds: 1 + _duration.inSeconds);
+    setState(() {});
   }
 }
