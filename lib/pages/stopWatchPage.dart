50229a53e1435778b8ed54357f2e0e4940b70309
Nasir-gaayte <slowboyz12@gmail.com>
2024-05-14 17:31:55 +0300

    14/5update6

--- lib/pages/stopWatchPage.dart
+++ lib/pages/stopWatchPage.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class StopWatchPage extends StatefulWidget {
@@ -21,17 +22,68 @@ class _StopWatchPageState extends State<StopWatchPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: buildTimer());
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              buildTimerContent(
+                  Text(
+                    _duration.inHours.toString(),
+                    style: TextStyle(
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25),
+                  ),
+                  'hour'),
+              buildTimerContent(
+                  Text(
+                    _duration.inMinutes.toString(),
+                    style: TextStyle(
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25),
+                  ),
+                  'minute'),
+              buildTimerContent(
+                  Text(
+                    _duration.inSeconds.toString(),
+                    style: TextStyle(
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25),
+                  ),
+                  'seconds'),
+            ],
+          ),
+        ),
+        buildTimer(),
+        ElevatedButton.icon(
+            onPressed: () {}, icon: Icon(Icons.pause), label: Text('Pause')),
+        ElevatedButton.icon(
+            onPressed: () {
+              setState(() {
+                _timer?.cancel();
+              });
+            },
+            icon: Icon(Icons.stop),
+            label: Text('Stop')),
+      ],
+    ));
   }
 
   buildTimer() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text(_duration.inHours.toString()),
-        Text(_duration.inMinutes.toString()),
+        // Text(_duration.inHours.toString()),
+        // Text(_duration.inMinutes.toString()),
         Text(_duration.inSeconds.toString()),
-        Text(_duration.inMicroseconds.toString()),
+        // Text(_duration.inMicroseconds.toString()),
       ],
     );
   }
@@ -49,5 +101,24 @@ class _StopWatchPageState extends State<StopWatchPage> {
   void addTimer() {
     _duration = Duration(seconds: 1 + _duration.inSeconds);
     setState(() {});
+  }
+
+  buildTimerContent(Text text, String text2) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          height: 85,
+          width: 85,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.yellowAccent,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: text,
+        ),
+        Text(text2),
+      ],
+    );
   }
 }
